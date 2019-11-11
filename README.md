510590441377cf95216a6e9fd293117f8377e9aa
김은택 <etkim@wemakeprice.com>
2019-11-11 15:27:04 +0900

    chapter4 추가

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Reactive Programming
 
-1차 강의 시 사용했던 ppt 첨부합니다: [링크](http://stash.wemakeprice.com/projects/EDU/repos/reactive/browse/docs/ReactiveProgramming.pptx)
+1차 강의 시 사용했던 ppt 첨부합니다: [링크](http://stash.wemakeprice.com/projects/EDU/repos/reactive/browse/docs/ReactiveProgramming_1st.pptx)
+2차 강의 시 사용했던 ppt 첨부합니다: [링크](http://stash.wemakeprice.com/projects/EDU/repos/reactive/browse/docs/ReactiveProgramming_2nd.pptx)
---
+++ src/main/java/com/example/reactive/chapter4/webflux/TemperatureRestController.java
@@ -0,0 +1,47 @@
+package com.example.reactive.chapter4.webflux;
+
+import com.example.reactive.chapter2.sse.Temperature;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Random;
+
+@RestController
+@SpringBootApplication
+public class TemperatureRestController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureRestController.class, args);
+    }
+
+    @GetMapping(value = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+
+        return Flux.interval(Duration.ofSeconds(1)).map(seq -> "message: " + seq + ", time: " + LocalTime.now().toString());
+    }
+
+    @GetMapping("/stream-see")
+    public Flux<ServerSentEvent<String>> streamSee() {
+
+        return Flux.interval(Duration.ofSeconds(1)).map(seq -> ServerSentEvent.<String>builder()
+                .data("message: " + seq + ", time: " + LocalTime.now().toString())
+                .build());
+    }
+
+    @GetMapping("/stream-temperature")
+    public Flux<ServerSentEvent<Temperature>> streamTemperature() {
+
+        Random random = new Random();
+        return Flux.interval(Duration.ofSeconds(2)).map(seq ->
+                ServerSentEvent.<Temperature>builder()
+                        .data(new Temperature(random.nextGaussian() * 10 + 16))
+                        .build());
+    }
+}
